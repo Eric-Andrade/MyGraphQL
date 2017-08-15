692cc8a0e597b1267f7842ce87182a2dd3aa06e9
Eric Torres Andrade <erictorresandrade.1@gmail.com>
2017-08-14 22:56:02 -0500

    Semillas v1.0 GraphQL JavaScript(Express) y Python(Django)

---
+++ Javascript/index.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import expressGraphQL from 'express-graphql'
+
+const app = express(),
+      port = process.env.PORT || 4000;
+const schema = require('./schema/schema');
+
+app
+    .use('/graphql', expressGraphQL({
+        schema,
+        graphiql: true
+    }))
+    .listen(port, () => console.log(`GraphQL running on port ${port}...`))
